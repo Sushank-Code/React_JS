505383da9cd4bd6cba17f892cdf99301797f74ad
Sushank-Code <sushanklamsal7979@gmail.com>
2025-06-16 23:46:52 +0545

    3rd commit

---
+++ 03_counter_hooks/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+
+  let counter_value = 0;
+  let [counter, setCounter] = useState(counter_value)
+  function addvalue(){
+      setCounter(counter+1)
+  }
+  function subvalue(){
+      if(counter <= 0){
+        setCounter(counter === 0)
+        
+      }
+      else{
+        setCounter(counter-1)
+      }
+  }
+  return (
+    <>
+      <h1> Counter Project</h1>
+      <h2> Counter Value : { counter }</h2>
+      <button onClick={addvalue}> Add Value </button>
+      <hr />
+      <button onClick={subvalue}> Subtract Value </button>
+    </>
+  )
+}
+
+export default App
