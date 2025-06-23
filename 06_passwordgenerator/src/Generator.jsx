4de93d9678e8f1a54315849c8249099f607817cb
Sushank-Code <sushanklamsal7979@gmail.com>
2025-06-23 22:06:13 +0545

    pw_generator

--- 06_passwordgenerator/src/Generator.jsx
+++ 06_passwordgenerator/src/Generator.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react'
+import React, { useState, useCallback, useEffect, useRef } from 'react'
 
 const Generator = () => {
     const [length, setLength] = useState(8)
@@ -13,20 +13,38 @@ const Generator = () => {
             if (numallow) str += "0123456789"
             if (challow) str += "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-            for (const i of str) {
+            for (let i = 1; i <= length; i++) {
                 let char = Math.floor(Math.random() * str.length + 1)
                 pass += str.charAt(char)
             }
+            setPassword(pass)
         },
         [length, numallow, challow, setPassword],
     )
 
+    useEffect(() => {
+        pwGenerator()
+    }, [length, numallow, challow, pwGenerator])
+
+    const pwref = useRef(null)
+
+    const copyPasswordToClipboard = useCallback(
+      () => {
+        pwref.current?.select()
+        // pwref.current?.setSelectionRange(0,5)  // For giving selection range
+        window.navigator.clipboard.writeText(password)
+      },
+      [password],
+    )
+    
+
     return (
         <>
-            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
-                <h1 className='text-white text-center my-3'>Password generator</h1>
+            <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-50 bg-gray-800 text-orange-500">
+                <h1 className='text-white text-center my-2'>Password Generator</h1>
 
-                // input and button
+                {/* input and button */}
+
                 <div className="flex shadow rounded-lg overflow-hidden mb-4">
                     <input
                         type="text"
@@ -34,14 +52,15 @@ const Generator = () => {
                         className='outline-none w-full py-1 px-3 bg-white'
                         placeholder='password'
                         readOnly
+                        ref={pwref}
                     />
                     <button
-                        // onClick={copyPasswordToClipboard}
-                        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
+                        onClick={copyPasswordToClipboard}
+                        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0 cursor-pointer hover:bg-sky-700'
                     >copy</button>
                 </div>
 
-                //slider & checkbox
+                {/* slider & checkbox */}
 
                 <div className='flex text-sm gap-x-2'>
                     <div className='flex items-center gap-x-1'>
@@ -50,13 +69,35 @@ const Generator = () => {
                             max={30}
                             value={length}
                             className='cursor-pointer'
-                            onChange={()=>{}}
+                            onChange={(e) => setLength(e.target.value)}
                         />
                         <label>Length: {length}</label>
                     </div>
+
+                    <div className="flex items-center gap-x-1">
+                        <input
+                            type="checkbox"
+                            defaultChecked={numallow}
+                            id="numberInput"
+                            onChange={() =>
+                                setNumallow((prev) => !prev)
+                            }
+                        />
+                        <label htmlFor="numberInput">Numbers</label>
+                    </div>
+                    <div className="flex items-center gap-x-1">
+                        <input
+                            type="checkbox"
+                            defaultChecked={challow}
+                            id="charInput"
+                            onChange={() =>
+                                setChallow((prev) => !prev)
+                            }
+                        />
+                        <label htmlFor="charInput">Character</label>
+                    </div>
                 </div>
             </div>
-
         </>
     )
 }
