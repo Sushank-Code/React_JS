ef11ceb806d8e682a65147a52a8878775014af40
Sushank-Code <sushanklamsal7979@gmail.com>
2025-07-20 23:46:11 +0545

    redux

--- 12_reduxtlk/src/features/todo/todoSlice.js
+++ 12_reduxtlk/src/features/todo/todoSlice.js
@@ -11,19 +11,19 @@ export const todoSlice = createSlice({
     name: 'todo',
     initialState,
     reducers: {
-        addTodo: (state , action) => {
+        addTodo: (state, action) => {
             const todo = {
-                id : nanoid(),
-                text : action.payload
+                id: nanoid(),
+                text: action.payload
             }
             state.todos.push(todo)
         },
-        removeTodo: (state ,action) => { 
+        removeTodo: (state, action) => {
             state.todos = state.todos.filter((todo) => todo.id !== action.payload)
         },
     }
 })
 
-export const {addTodo,removeTodo}  = todoSlice.actions
+export const { addTodo, removeTodo } = todoSlice.actions
 
 export default todoSlice.reducer
